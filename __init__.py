0faf0cbd3d1a08b55279773ae0c813ae853a8339
diklag <dikla.gelbard@weizmann.ac.il>
2017-06-08 10:33:52 +0300

    first version

---
+++ __init__.py
@@ -0,0 +1,5 @@
+import os
+
+# Parent directory
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+wells_cells_file = '/home/labs/amit/diklag/files/wells_cells_SB102.txt'
